0f3f47c8dc2b8e33ef43c212e029ad5cbfa2357d
WujecDamian <wujecdamian12@gmail.com>
2026-03-28 23:12:03 +0100

    chore: stash before revert

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import "./styles.css";
 import { knightMoves } from "./scripts/knightMoves.js";
 
 let knight = knightMoves();
-knight.findShortest([4, 0], [3, 7]);
+/* knight.findShortest([4, 0], [3, 7]); */
--- src/scripts/knightMoves.js
+++ src/scripts/knightMoves.js
@@ -1,10 +1,24 @@
+import { ids } from "webpack";
 import { visited } from "./visited.js";
 
 export function knightMoves() {
   return {
     findShortest(startPoint, endPoint) {
-      console.table(`Start point: ${startPoint}`);
+      /*       console.table(`Start point: ${startPoint}`);
       console.log(`End point: ${endPoint}`);
+      function move(startPoint, endPoint) {
+        if (startPoint === endPoint) {
+          console.log("Found shortest path!");
+        }
+        /!we have at most 8 avaible moves:
+        console.log(`Move left: ${startPoint[0] - 1}`);
+
+        //down-left
+        if (startPoint[0] - 1 < 0) {
+          console.log(`nope`);
+          move(startPoint, endPoint);
+        }
+      } */
     },
   };
 }
