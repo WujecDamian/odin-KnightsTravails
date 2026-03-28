9f38e51a60b80df5776b5a39acf8621dad1a70d7
WujecDamian <wujecdamian12@gmail.com>
2026-03-28 22:15:52 +0100

    feat: create knighMoves factory with findShortest function

--- src/scripts/knightMoves.js
+++ src/scripts/knightMoves.js
@@ -1,1 +1,10 @@
-export function knightMoves(startPoint, endPoint) {}
+import { visited } from "./visited.js";
+
+export function knightMoves() {
+  return {
+    findShortest(startPoint, endPoint) {
+      console.table(`Start point: ${startPoint}`);
+      console.log(`End point: ${endPoint}`);
+    },
+  };
+}
