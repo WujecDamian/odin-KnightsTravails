42bdda7a638813583623755e7946615cbaddcfe3
WujecDamian <wujecdamian12@gmail.com>
2026-03-29 21:53:13 +0200

    feat: add KnightMoves findshortest function

--- src/scripts/knightMoves.js
+++ src/scripts/knightMoves.js
@@ -4,21 +4,48 @@ import { visited } from "./visited.js";
 export function knightMoves() {
   return {
     findShortest(startPoint, endPoint) {
-      /*       console.table(`Start point: ${startPoint}`);
-      console.log(`End point: ${endPoint}`);
-      function move(startPoint, endPoint) {
-        if (startPoint === endPoint) {
-          console.log("Found shortest path!");
+      let visitedArr = visited();
+      function moveKnight(startPoint, endPoint) {
+        if (startPoint[0] + 1 < 8 && startPoint[1] + 2 < 8) {
+          visitedArr.logVisited();
         }
-        /!we have at most 8 avaible moves:
-        console.log(`Move left: ${startPoint[0] - 1}`);
+        if (visitedArr[0] != [startPoint[0] + 1, startPoint[1] + 2]) {
+          console.log(`${[startPoint[0] + 1, startPoint[1] + 2]}`);
+          visitedArr.add(startPoint[0] + 1, startPoint[1] + 2);
+          moveKnight([startPoint[0] + 1, startPoint[1] + 2], endPoint);
+        }
+      }
+      /* up left /
+        / if (startPoint[0] - 1 < 8 && startPoint[1] + 2 < 8) {
+          moveKnight([startPoint[0] - 1, startPoint[1] + 2], endPoint);
+        } /
+        / right up /
+        / if (startPoint[0] + 2 < 8 && startPoint[1] + 1 < 8) {
+          moveKnight([startPoint[0] + 2, startPoint[1] + 1], endPoint);
+        } /
+        / right down /
+        / if (startPoint[0] + 2 < 8 && startPoint[1] - 1 < 8) {
+          moveKnight([startPoint[0] + 2, startPoint[1] - 1], endPoint);
+        } /
 
-        //down-left
-        if (startPoint[0] - 1 < 0) {
-          console.log(`nope`);
-          move(startPoint, endPoint);
-        }
-      } */
+        //here  i get full stack error when there is too much if's
+        / down right /
+        / if (startPoint[0] + 1 < 8 && startPoint[1] - 2 < 8) {
+          moveKnight([startPoint[0] + 1, startPoint[1] - 2], endPoint);
+        } /
+        / down left /
+        / if (startPoint[0] - 1 < 8 && startPoint[1] - 2 < 8) {
+          moveKnight([startPoint[0] - 1, startPoint[1] - 2], endPoint);
+        } /
+        / left down /
+        / if (startPoint[0] - 2 < 8 && startPoint[1] - 1 < 8) {
+          moveKnight([startPoint[0] - 2, startPoint[1] - 1], endPoint);
+        } /
+        / left up /
+        / if (startPoint[0] - 2 < 8 && startPoint[1] + 1 < 8) {
+          moveKnight([startPoint[0] - 2, startPoint[1] + 1], endPoint);
+        } */
+      moveKnight(startPoint, endPoint);
     },
   };
 }
