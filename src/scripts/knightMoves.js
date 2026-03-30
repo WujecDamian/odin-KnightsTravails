fe05ee996cc011c0e36c7c98af898f5b88ff8f42
WujecDamian <wujecdamian12@gmail.com>
2026-03-30 09:53:36 +0200

    fix: fix closing bracket closing after first if in moveKnight

--- src/scripts/knightMoves.js
+++ src/scripts/knightMoves.js
@@ -4,11 +4,10 @@ export function knightMoves() {
   return {
     findShortest(startPoint, endPoint) {
       let visitedArr = visited();
-      moveKnight(startPoint, endPoint);
 
-      visitedArr.visitedArr.forEach((element) => {
+      /* visitedArr.visitedArr.forEach((element) => {
         moveKnight(element, endPoint);
-      });
+      }); */
 
       function moveKnight(startPoint, endPoint) {
         if (
@@ -17,7 +16,6 @@ export function knightMoves() {
           startPoint[0] + 1 >= 0 &&
           startPoint[1] + 2 >= 0
         ) {
-          visitedArr.logVisited();
           if (
             startPoint[0] + 1 === endPoint[0] &&
             startPoint[1] + 2 === endPoint[1]
@@ -29,140 +27,134 @@ export function knightMoves() {
             visitedArr.add(startPoint[0] + 1, startPoint[1] + 2);
           }
         }
-      }
 
-      if (
-        startPoint[0] - 1 < 8 &&
-        startPoint[1] + 2 < 8 &&
-        startPoint[0] - 1 >= 0 &&
-        startPoint[1] + 2 >= 0
-      ) {
-        visitedArr.logVisited();
         if (
-          startPoint[0] - 1 === endPoint[0] &&
-          startPoint[1] + 2 === endPoint[1]
+          startPoint[0] - 1 < 8 &&
+          startPoint[1] + 2 < 8 &&
+          startPoint[0] - 1 >= 0 &&
+          startPoint[1] + 2 >= 0
         ) {
-          console.log(`Found shortest path to ! ${endPoint}`);
+          if (
+            startPoint[0] - 1 === endPoint[0] &&
+            startPoint[1] + 2 === endPoint[1]
+          ) {
+            console.log(`Found shortest path to ! ${endPoint}`);
+          }
+          if (!visitedArr.isVisited([startPoint[0] - 1, startPoint[1] + 2])) {
+            console.log(`up left ${[startPoint[0] - 1, startPoint[1] + 2]}`);
+            visitedArr.add(startPoint[0] - 1, startPoint[1] + 2);
+          }
         }
-        if (!visitedArr.isVisited([startPoint[0] - 1, startPoint[1] + 2])) {
-          console.log(`up left ${[startPoint[0] - 1, startPoint[1] + 2]}`);
-          visitedArr.add(startPoint[0] - 1, startPoint[1] + 2);
-        }
-      }
-      / right up /;
-      if (
-        startPoint[0] + 2 < 8 &&
-        startPoint[1] + 1 < 8 &&
-        startPoint[0] + 2 >= 0 &&
-        startPoint[1] + 1 >= 0
-      ) {
-        visitedArr.logVisited();
+        / right up /;
         if (
-          startPoint[0] + 2 === endPoint[0] &&
-          startPoint[1] + 1 === endPoint[1]
+          startPoint[0] + 2 < 8 &&
+          startPoint[1] + 1 < 8 &&
+          startPoint[0] + 2 >= 0 &&
+          startPoint[1] + 1 >= 0
         ) {
-          console.log(`Found shortest path to ! ${endPoint}`);
+          if (
+            startPoint[0] + 2 === endPoint[0] &&
+            startPoint[1] + 1 === endPoint[1]
+          ) {
+            console.log(`Found shortest path to ! ${endPoint}`);
+          }
+          if (!visitedArr.isVisited([startPoint[0] + 2, startPoint[1] + 1])) {
+            console.log(`right up ${[startPoint[0] + 2, startPoint[1] + 1]}`);
+            visitedArr.add(startPoint[0] + 2, startPoint[1] + 1);
+          }
         }
-        if (!visitedArr.isVisited([startPoint[0] + 2, startPoint[1] + 1])) {
-          console.log(`right up ${[startPoint[0] + 2, startPoint[1] + 1]}`);
-          visitedArr.add(startPoint[0] + 2, startPoint[1] + 1);
-        }
-      }
-      / right down /;
-      if (
-        startPoint[0] + 2 < 8 &&
-        startPoint[1] - 1 < 8 &&
-        startPoint[0] + 2 >= 0 &&
-        startPoint[1] - 1 >= 0
-      ) {
-        visitedArr.logVisited();
+        / right down /;
         if (
-          startPoint[0] + 2 === endPoint[0] &&
-          startPoint[1] - 1 === endPoint[1]
+          startPoint[0] + 2 < 8 &&
+          startPoint[1] - 1 < 8 &&
+          startPoint[0] + 2 >= 0 &&
+          startPoint[1] - 1 >= 0
         ) {
-          console.log(`Found shortest path to ! ${endPoint}`);
+          if (
+            startPoint[0] + 2 === endPoint[0] &&
+            startPoint[1] - 1 === endPoint[1]
+          ) {
+            console.log(`Found shortest path to ! ${endPoint}`);
+          }
+          if (!visitedArr.isVisited([startPoint[0] + 2, startPoint[1] - 1])) {
+            console.log(`right down ${[startPoint[0] + 2, startPoint[1] - 1]}`);
+            visitedArr.add(startPoint[0] + 2, startPoint[1] - 1);
+          }
         }
-        if (!visitedArr.isVisited([startPoint[0] + 2, startPoint[1] - 1])) {
-          console.log(`right down ${[startPoint[0] + 2, startPoint[1] - 1]}`);
-          visitedArr.add(startPoint[0] + 2, startPoint[1] - 1);
-        }
-      }
-      /up left +1 -2/;
-      //here  i get full stack error when there is too much if's
-      if (
-        startPoint[0] + 1 < 8 &&
-        startPoint[1] - 2 < 8 &&
-        startPoint[0] + 1 >= 0 &&
-        startPoint[1] - 2 >= 0
-      ) {
-        visitedArr.logVisited();
+        /up left +1 -2/;
+        //here  i get full stack error when there is too much if's
         if (
-          startPoint[0] + 1 === endPoint[0] &&
-          startPoint[1] - 2 === endPoint[1]
+          startPoint[0] + 1 < 8 &&
+          startPoint[1] - 2 < 8 &&
+          startPoint[0] + 1 >= 0 &&
+          startPoint[1] - 2 >= 0
         ) {
-          console.log(`Found shortest path to ! ${endPoint}`);
+          if (
+            startPoint[0] + 1 === endPoint[0] &&
+            startPoint[1] - 2 === endPoint[1]
+          ) {
+            console.log(`Found shortest path to ! ${endPoint}`);
+          }
+          if (!visitedArr.isVisited([startPoint[0] + 1, startPoint[1] - 2])) {
+            console.log(`down right${[startPoint[0] + 1, startPoint[1] - 2]}`);
+            visitedArr.add(startPoint[0] + 1, startPoint[1] - 2);
+          }
         }
-        if (!visitedArr.isVisited([startPoint[0] + 1, startPoint[1] - 2])) {
-          console.log(`down right${[startPoint[0] + 1, startPoint[1] - 2]}`);
-          visitedArr.add(startPoint[0] + 1, startPoint[1] - 2);
-        }
-      }
-      / down left -1 -2/;
-      if (
-        startPoint[0] - 1 < 8 &&
-        startPoint[1] - 2 < 8 &&
-        startPoint[0] - 1 >= 0 &&
-        startPoint[1] - 2 >= 0
-      ) {
-        visitedArr.logVisited();
+        / down left -1 -2/;
         if (
-          startPoint[0] - 1 === endPoint[0] &&
-          startPoint[1] - 2 === endPoint[1]
+          startPoint[0] - 1 < 8 &&
+          startPoint[1] - 2 < 8 &&
+          startPoint[0] - 1 >= 0 &&
+          startPoint[1] - 2 >= 0
         ) {
-          console.log(`Found shortest path to ! ${endPoint}`);
+          if (
+            startPoint[0] - 1 === endPoint[0] &&
+            startPoint[1] - 2 === endPoint[1]
+          ) {
+            console.log(`Found shortest path to ! ${endPoint}`);
+          }
+          if (!visitedArr.isVisited([startPoint[0] - 1, startPoint[1] - 2])) {
+            console.log(`down left ${[startPoint[0] - 1, startPoint[1] - 2]}`);
+            visitedArr.add(startPoint[0] - 1, startPoint[1] - 2);
+          }
         }
-        if (!visitedArr.isVisited([startPoint[0] - 1, startPoint[1] - 2])) {
-          console.log(`down left ${[startPoint[0] - 1, startPoint[1] - 2]}`);
-          visitedArr.add(startPoint[0] - 1, startPoint[1] - 2);
-        }
-      }
-      / left down -2 -1/;
-      if (
-        startPoint[0] - 2 < 8 &&
-        startPoint[1] - 1 < 8 &&
-        startPoint[0] - 2 >= 0 &&
-        startPoint[1] - 1 >= 0
-      ) {
-        visitedArr.logVisited();
+        / left down -2 -1/;
         if (
-          startPoint[0] - 2 === endPoint[0] &&
-          startPoint[1] - 1 === endPoint[1]
+          startPoint[0] - 2 < 8 &&
+          startPoint[1] - 1 < 8 &&
+          startPoint[0] - 2 >= 0 &&
+          startPoint[1] - 1 >= 0
         ) {
-          console.log(`Found shortest path to ! ${endPoint}`);
+          if (
+            startPoint[0] - 2 === endPoint[0] &&
+            startPoint[1] - 1 === endPoint[1]
+          ) {
+            console.log(`Found shortest path to ! ${endPoint}`);
+          }
+          if (!visitedArr.isVisited([startPoint[0] - 2, startPoint[1] - 1])) {
+            console.log(`left down ${[startPoint[0] - 2, startPoint[1] - 1]}`);
+            visitedArr.add(startPoint[0] - 2, startPoint[1] - 1);
+          }
         }
-        if (!visitedArr.isVisited([startPoint[0] - 2, startPoint[1] - 1])) {
-          console.log(`left down ${[startPoint[0] - 2, startPoint[1] - 1]}`);
-          visitedArr.add(startPoint[0] - 2, startPoint[1] - 1);
-        }
-      }
-      / left up -2 +1/;
-      if (
-        startPoint[0] - 2 < 8 &&
-        startPoint[1] + 1 < 8 &&
-        startPoint[0] - 2 >= 0 &&
-        startPoint[1] + 1 >= 0
-      ) {
-        visitedArr.logVisited();
+        / left up -2 +1/;
         if (
-          startPoint[0] - 2 === endPoint[0] &&
-          startPoint[1] + 1 === endPoint[1]
+          startPoint[0] - 2 < 8 &&
+          startPoint[1] + 1 < 8 &&
+          startPoint[0] - 2 >= 0 &&
+          startPoint[1] + 1 >= 0
         ) {
-          console.log(`Found shortest path to ! ${endPoint}`);
+          if (
+            startPoint[0] - 2 === endPoint[0] &&
+            startPoint[1] + 1 === endPoint[1]
+          ) {
+            console.log(`Found shortest path to ! ${endPoint}`);
+          }
+          if (!visitedArr.isVisited([startPoint[0] - 2, startPoint[1] + 1])) {
+            console.log(`left up ${[startPoint[0] - 2, startPoint[1] + 1]}`);
+            visitedArr.add(startPoint[0] - 2, startPoint[1] + 1);
+          }
         }
-        if (!visitedArr.isVisited([startPoint[0] - 2, startPoint[1] + 1])) {
-          console.log(`left up ${[startPoint[0] - 2, startPoint[1] + 1]}`);
-          visitedArr.add(startPoint[0] - 2, startPoint[1] + 1);
-        }
+        visitedArr.logVisited();
       }
     },
   };
