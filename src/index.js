0a17654977de38cc6db4eb2cdad036650eb09c9b
WujecDamian <wujecdamian12@gmail.com>
2026-03-30 08:49:32 +0200

    feat: add initial index.js code (paste from reverted)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 //put inside src/index.js
-import './styles.css'
+import "./styles.css";
+import { knightMoves } from "./scripts/knightMoves.js";
+
+let knight = knightMoves();
+/* knight.findShortest([4, 0], [3, 7]); */
