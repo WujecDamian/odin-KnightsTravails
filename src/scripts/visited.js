cfa69d80fb2762f9c25ff6735accaa902b525282
WujecDamian <wujecdamian12@gmail.com>
2026-03-30 09:11:20 +0200

    feat: add isVisited function to array

--- src/scripts/visited.js
+++ src/scripts/visited.js
@@ -7,5 +7,15 @@ export function visited() {
     logVisited() {
       console.table(this.visitedArr);
     },
+    isVisited(coordinates) {
+      console.log("Isn't visited");
+      for (let i = 0; i < this.visitedArr.length; i++) {
+        if (this.visitedArr[i] === coordinates) {
+          return true;
+        }
+      }
+
+      return false;
+    },
   };
 }
